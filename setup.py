59d575aa128d6bed8aec1661b62a39943ea291c6
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2021-01-19 00:28:55 +0900

    version 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pocpy",
-    version="0.0.1",
+    version="0.1.0",
     license="MIT",
     author="Daisuke Kobayashi",
     author_email="daisuke@daisukekobayashi.com",
